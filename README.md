e8970d48d97a5b62a96bbcbc6f06f866215f3708
Rob Gietema <rob@fourdigits.nl>
2013-02-15 17:02:33 +0100

    Initial checkin.

---
+++ README.md
@@ -0,0 +1,6 @@
+# twist - Twist is a Multi Player Javascript Game #
+
+[![Build status](https://secure.travis-ci.org/robgietema/twist.png?branch=master)](http://travis-ci.org/robgietema/twist/)
+
+**twist** is a Javascript game which uses a central screen and a mobile phone
+or tablet per user to play.
---
+++ test/twist-test.js
@@ -0,0 +1,14 @@
+/*global assert: false, refute: false */
+var buster = require("buster"),
+    huddle = require("../lib/twist.js"),
+    resources;
+
+buster.testCase('Twist', {
+    setUp: function () {
+        // resources = new twist.Twist();
+    },
+
+    "Dummy": function () {
+        assert(true);
+    }
+});
